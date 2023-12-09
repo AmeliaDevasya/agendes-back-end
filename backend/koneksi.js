a190137f603e647e40862ccc9f78e2c49a3160b5
AmeliaDevasya <amel.dvsy@gmail.com>
2023-12-09 21:25:41 +0800

    update koneksi

--- backend/koneksi.js
+++ backend/koneksi.js
@@ -3,10 +3,10 @@ var mysql = require('mysql');
 
 // buat koneksi database
 const conn = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'agendes',
+  host: 'bomkym8ogvvgv3gbcyef-mysql.services.clever-cloud.com',
+  user: 'usgvmjho2t2lasf0',
+  password: '5UgkPE6Xnmhi4jdu7xzZ',
+  database: 'bomkym8ogvvgv3gbcyef',
 });
 
 conn.connect((err) => {
