ea482a264e25b310fdc670288069294f0b6e4d29
AmeliaDevasya <amel.dvsy@gmail.com>
2023-12-09 23:14:08 +0800

    open komentar add agenda

--- backend/controller.js
+++ backend/controller.js
@@ -39,37 +39,37 @@ exports.getAgendabById = function (req, res) {
 
 //menambahkan data Agenda
 exports.addNewAgenda = function (req, res) {
-    // var nama_kegiatan = req.body.nama_kegiatan;
-    // var deskripsi_kegiatan = req.body.deskripsi_kegiatan;
-    // var tempat_kegiatan = req.body.tempat_kegiatan;
-    // var jenis_kegiatan = req.body.jenis_kegiatan;
-    // var waktu = req.body.waktu;
-    // var tambahan = req.body.tambahan;
+    var nama_kegiatan = req.body.nama_kegiatan;
+    var deskripsi_kegiatan = req.body.deskripsi_kegiatan;
+    var tempat_kegiatan = req.body.tempat_kegiatan;
+    var jenis_kegiatan = req.body.jenis_kegiatan;
+    var waktu = req.body.waktu;
+    var tambahan = req.body.tambahan;
     
-    //Validasi apakah properti 'nama_kegiatan' , 'tempat_kegiatan' dan 'waktu' ada pada request body
-    // if (!nama_kegiatan || !tempat_kegiatan || !waktu) {
-    //     const response = {
-    //         status: 'fail',
-    //         message: 'Gagal menambahkan agenda. Mohon isi nama, tempat, dan waktu kegiatan!',
-    //     };
-    //     res.status(400).json(response);
-    //     return;
-    // }
+    // Validasi apakah properti 'nama_kegiatan' , 'tempat_kegiatan' dan 'waktu' ada pada request body
+    if (!nama_kegiatan || !tempat_kegiatan || !waktu) {
+        const response = {
+            status: 'fail',
+            message: 'Gagal menambahkan agenda. Mohon isi nama, tempat, dan waktu kegiatan!',
+        };
+        res.status(400).json(response);
+        return;
+    }
 
-    // var foto_kegiatan = req.file ? '/uploads/' + req.file.filename : null;
+    var foto_kegiatan = req.file ? '/uploads/' + req.file.filename : null;
     
-    // connection.query('INSERT INTO kegiatan_desa (nama_kegiatan,deskripsi_kegiatan,tempat_kegiatan,jenis_kegiatan,waktu,tambahan,foto_kegiatan) VALUES(?,?,?,?,?,?,?)',
-    //     [nama_kegiatan, deskripsi_kegiatan, tempat_kegiatan, jenis_kegiatan, waktu, tambahan, foto_kegiatan], function (error, rows, fields) {
-    //         if (error) {
-    //             console.log(error);
-    //         } else {
-    //             const successResponse = {
-    //                 status: 'success',
-    //                 message: 'Agenda berhasil ditambahkan',
-    //             };
-    //             res.status(201).json(successResponse);
-    //         }
-    //     });
+    connection.query('INSERT INTO kegiatan_desa (nama_kegiatan,deskripsi_kegiatan,tempat_kegiatan,jenis_kegiatan,waktu,tambahan,foto_kegiatan) VALUES(?,?,?,?,?,?,?)',
+        [nama_kegiatan, deskripsi_kegiatan, tempat_kegiatan, jenis_kegiatan, waktu, tambahan, foto_kegiatan], function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                const successResponse = {
+                    status: 'success',
+                    message: 'Agenda berhasil ditambahkan',
+                };
+                res.status(201).json(successResponse);
+            }
+        });
 };
 
 //mengubah data agenda berdasarkan id
